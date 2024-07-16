a9816a0133ec7314e3d46b8550ff3a8983d46cae
WebBrandon423 <brandonkimboss@gmail.com>
2024-07-16 00:07:09 -0400

    Login Form
    Developed a basic login form system template
    -No CSS and page design as of now
    - Minor error regarding the radio buttons

--- routing.py
+++ routing.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, RadioField, SubmitField, validators 
+from wtforms import StringField, RadioField, SubmitField
 from wtforms.validators import DataRequired
 app = Flask(__name__) 
 app.config['SECRET_KEY'] = 'placeholder'
 class SignUpForms(FlaskForm):
-	firstname = StringField("First Name: ") 
-	lastname = StringField("Last Name: ")
-	username = StringField("Username: ")
-	password = StringField("Password: ")
-	grade = RadioField("Grade Level: ", choices=[("ninth", "9th"), ("tenth", "10th"), ("eleventh, 11th"), ("twelveth, 12th")])
+	firstname = StringField("First Name: ",validators=[DataRequired()]) 
+	lastname = StringField("Last Name: ", validators=[DataRequired()])
+	username = StringField("Username: ", validators=[DataRequired()])
+	password = StringField("Password: ", validators=[DataRequired()])
+	grade = RadioField("Grade Level: ", choices=[("ninth", "9th"), ("tenth", "10th"), ("eleventh, 11th"), ("twelveth, 12th")], validators=[DataRequired()])
 	submit = SubmitField("Submit")
 @app.route('/') 
 def index(): 
 	return render_template('front.html')
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/create', methods = ['GET', 'POST'])
 def login():
 	firstname = False 
 	lastname = False
@@ -28,7 +28,7 @@ def login():
 		username = form.username.data
 		password = form.password.data
 		grade = form.grade.data 
-	return render_template('signin.html', firstname = firstname, lastname = lastname, username = username, password = password, grade = grade)
+	return render_template('signin.html', firstname = firstname, lastname = lastname, username = username, password = password, grade = grade, form = form)
 @app.errorhandler(404) 
 def page_not_found(e): 
 	return render_template('error.html'), 404
